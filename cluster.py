99a9a1b85b8e15de5307fe0bca7e2ccc4d5436ed
rahman taufik <rahmantaufik27@gmail.com>
2023-09-14 15:43:35 +0700

    restructure

--- cluster.py
+++ cluster.py
@@ -14,6 +14,8 @@ import re
 import time
 import umap
 import sys
+import csv
+from datetime import date
 from gensim.models import LsiModel
 from gensim import corpora
 import nltk
@@ -33,20 +35,17 @@ df_raw = pd.read_csv("data/Final Dataset Fix.csv")
 # print(len(df_raw))
 # print(df_raw["divisions"].isnull().sum())
 # print(100*(df_raw["divisions"].isnull().sum()/len(df_raw)))
-
+# delete the null values based on divisions
+df_raw = df_raw.dropna(subset=['divisions'])
+total_data = len(df_raw)
 fac = ['FKIP', 'FMIPA', 'FEB', 'FT', 'FP', 'FISIP', 'FK', 'FH']
+# print(df_raw.isnull().sum())
+# df_raw.info()
+# print(df_raw.head())
+# sys.exit()
 
-for f in fac:
-  df_raw = df_raw.dropna(subset=['divisions'])
-  df = df_raw[df_raw["divisions"].str.contains(f)]
-  print("FKIP")
-  print(df["divisions"].unique())
-  print("Jumlah data ", len(df))
-
-sys.exit()
-
+# MODELING / CLUSTERING DATA
 def modeling_cluster(df):
-  # MODELING / CLUSTERING DATA
   text_raw = df["text_stemmed_stopword_without_number"].to_numpy()
   # vectorize
   vectorizer = TfidfVectorizer(use_idf=True, smooth_idf=True, sublinear_tf=False, ngram_range=(1,3))
@@ -61,6 +60,7 @@ def modeling_cluster(df):
   visualizer.fit(X)
   # clustering the data
   k = visualizer.elbow_value_
+  # print("number of cluster:", k)
   kmeans = KMeans(n_clusters = k, random_state=100)
   kmeans.fit(X)
   df["kluster"] = kmeans.labels_
@@ -69,14 +69,14 @@ def modeling_cluster(df):
   ss = silhouette_score(X, kmeans.labels_)
   sc = calinski_harabasz_score(X, kmeans.labels_)
   sd = davies_bouldin_score(X, kmeans.labels_)
-  print(f"Silhoutte score: {ss}")
-  print(f"Calonski Harabasz Score: {sc}")
-  print(f"Davies Bouldin Score: {sd}")
+  # print(f"Silhoutte score: {ss}")
+  # print(f"Calonski Harabasz Score: {sc}")
+  # print(f"Davies Bouldin Score: {sd}")
 
-  return clusters, ss, sc, sd
+  return clusters, k, ss, sc, sd, df
 
+# VISUALIZATION THE CLUSTER
 def visualizing_cluster(X, clusters):
-  # VISUALIZATION THE CLUSTER
   U, S, V = randomized_svd(X, n_components=7)
   X_topics=U*S
   color = ['lightcoral', 'darkorange', 'olive', 'teal', 'violet', 'skyblue', 'red']
@@ -89,8 +89,8 @@ def visualizing_cluster(X, clusters):
               )
   plt.show()
 
+# IDENTIFYING TOPIC
 def identifying_topic(df, k):
-  # IDENTIFYING TOPIC
   doc_clean = df["text_stemmed_stopword_without_number"]
   doc_clean_set = [d.split() for d in doc_clean]
   dictionary = corpora.Dictionary(doc_clean_set)
@@ -100,3 +100,25 @@ def identifying_topic(df, k):
   list_topic = lsamodel.print_topics(num_topics=k, num_words=words)
   
   return list_topic
+
+# EXPORT TO CSV
+csv_file = open('result/cluster_information_per_division.csv', 'w', newline="", encoding='utf-8') 
+writer = csv.writer(csv_file, sys.stdout, lineterminator='\n')
+writer.writerow(["faculty", "faculty_unique_relation", "total_data", "percentage", "cluster_number", "silhoutte_score", "calonski_harabasz_score", "davies_bouldin_score", "list_topic"])
+# create new dataframe
+dict_new = {}
+for f in fac:
+  df = df_raw[df_raw["divisions"].str.contains(f)]
+  clusters, k, ss, sc, sd, df_cluster = modeling_cluster(df)
+  list_topic = identifying_topic(df_cluster, k)
+  print(f)
+  dict_new["faculty"] = f
+  dict_new["faculty_unique_relation"] = str(df["divisions"].unique().tolist()).replace('"', '')
+  dict_new["total_data"] = int(len(df))
+  dict_new["percentage"] = round(100*(len(df)/total_data), 2)
+  dict_new["cluster_number"] = int(k)
+  dict_new["silhoutte_score"] = float(ss)
+  dict_new["calonski_harabasz_score"] = float(sc)
+  dict_new["davies_bouldin_score"] = float(sd)
+  dict_new["list_topic"] = list_topic
+  writer.writerow(dict_new.values())
